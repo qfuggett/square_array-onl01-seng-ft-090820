13937d82a0071c7dd8b34b8e90e27538763fb8a3
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-10 22:48:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-  array.each do |number| 
-    new_number = number ** 2
+  array.each do |number| ** 2
+    #number ** 2
+    new_number = number
     return new_number
   end
 end
